0e5ef59e3a15ce4aea22c7d178aecad206ef909c
vitorSantanaDev <vitorsantana.developer@gmail.com>
2024-10-16 09:09:37 -0300

    revert: removing styled-componentes from boilerplate

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,10 +1,8 @@
 import React from 'react'
-import GlobalStyles from '../src/styles/global'
 
 export const decorators = [
   (Story) => (
     <>
-      <GlobalStyles />
       <Story />
     </>
   )
--- jest.config.js
+++ jest.config.js
@@ -14,12 +14,5 @@ module.exports = {
   modulePaths: ['<rootDir>/src/'],
   transform: {
     '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }]
-  },
-  // https://github.com/styled-components/styled-components/issues/4081
-  // v6 of styled-components doesn't inject styles in test environment
-  // we should to force it to use the browser version
-  moduleNameMapper: {
-    '^styled-components':
-      'styled-components/dist/styled-components.browser.cjs.js'
   }
 }
