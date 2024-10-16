6eba001b4cd6229039415ff8b4941bd2e087c42d
vitorSantanaDev <vitorsantana.developer@gmail.com>
2024-10-16 11:06:33 -0300

    chore: removing dead code

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,6 +1,6 @@
 const config = {
   staticDirs: ['../public'],
-  stories: ['../src/components/**/stories.tsx'],
+  stories: ['../src/app/components/**/stories.tsx'],
   addons: ['@storybook/addon-essentials'],
   framework: {
     name: '@storybook/nextjs',
