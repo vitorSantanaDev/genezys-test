eb6da88a60eb6bb8542a66170eea0b637088c3ce
vitorSantanaDev <vitorsantana.developer@gmail.com>
2024-10-16 17:25:10 -0300

    feat: finalizing the register screen

--- src/app/auth/register/page.tsx
+++ src/app/auth/register/page.tsx
@@ -1,14 +1,20 @@
 'use client'
 
+import Link from 'next/link'
+
 import { useForm } from 'react-hook-form'
+import { useRouter } from 'next/navigation'
+import { useEffect, useReducer } from 'react'
 import { zodResolver } from '@hookform/resolvers/zod'
 
-import { RegisterFieldsInterface, RegisterSchema } from './resgister.defs'
-
+import { Routing } from '@/app/enum/routes.enum'
 import { Button } from '@/app/components/Button/Button'
+import { Loading } from '@/app/components/Loading/Loading'
 import { getAddress } from '@/app/services/address.service'
 import { TextInput } from '@/app/components/Input/TextInput'
 import { useUserContext } from '@/app/context/user/useUserContext'
+import { RegisterFieldsInterface, RegisterSchema } from './resgister.defs'
+import { toast } from 'react-toastify'
 
 export default function Register() {
   const {
@@ -23,17 +29,43 @@ export default function Register() {
     mode: 'onChange'
   })
 
-  const { actions: userContextActions } = useUserContext()
+  const router = useRouter()
+
+  const [isLoadingUser, toggleIsLoadingUser] = useReducer((prev) => !prev, true)
+
+  const {
+    state: { loggedUser },
+    actions: { setLoggedUser, setUser, getLoggedUserFromLocalStorage }
+  } = useUserContext()
 
   const onSubmit = async (data: RegisterFieldsInterface) => {
-    userContextActions.setUser({
-      user: {
+    try {
+      const payload = {
         name: data.name,
         email: data.email,
         password: data.password
       }
-    })
-    reset()
+
+      setUser(payload)
+      setLoggedUser(payload)
+      toast.success('Usuário criado com sucesso!')
+
+      reset({
+        uf: '',
+        name: '',
+        city: '',
+        email: '',
+        zipcode: '',
+        address: '',
+        password: '',
+        neighborhood: '',
+        confirmPassword: ''
+      })
+
+      router.push(Routing.HOME)
+    } catch (err) {
+      console.error(err)
+    }
   }
 
   const populateAddressInfo = async () => {
@@ -55,22 +87,51 @@ export default function Register() {
     })()
   }
 
+  useEffect(() => {
+    ;(async () => {
+      await getLoggedUserFromLocalStorage?.()
+      toggleIsLoadingUser()
+    })()
+  }, [])
+
+  useEffect(() => {
+    if (loggedUser) {
+      router.replace(Routing.HOME)
+    }
+  }, [loggedUser])
+
+  if (isLoadingUser) {
+    return <Loading />
+  }
+
   return (
-    <main className="bg-background-primary  min-h-screen">
-      <div className="p-4">
+    <main className="bg-background-primary  min-h-screen w-full flex flex-col justify-center">
+      <div className="px-16 py-8 w-full">
+        <Link href={Routing.LOGIN}>
+          <p className="text-content-primary text-right text-text-small cursor-pointer">
+            Já tem uma conta?{' '}
+            <span className="text-accent-brand">Acessar conta</span>
+          </p>
+        </Link>
+        <h1 className="text-content-primary text-left text-2xl mt-4 mb-2">
+          Criar Conta
+        </h1>
+        <div className="flex items-center justify-between"></div>
         <TextInput
           name="name"
           label="Nome"
           control={control}
           placeholder="Digite seu nome completo"
         />
+        <br />
         <TextInput
           name="email"
           label="E-mail"
           control={control}
           placeholder="Digite seu e-mail"
         />
-        <div className="min-w-max flex flex-col">
+        <br />
+        <div className="w-full">
           <h2 className="text-content-primary text-text-large mt-4 mb-2">
             Endereço
           </h2>
@@ -82,20 +143,23 @@ export default function Register() {
           />
           <br />
           <div className="flex items-center gap-4">
-            <div className="flex flex-col gap-4">
+            <div className="w-6/12">
               <TextInput name="address" control={control} placeholder="Rua" />
+              <br />
               <TextInput
                 name="neighborhood"
                 control={control}
                 placeholder="Bairro"
               />
             </div>
-            <div className="flex flex-col gap-4">
+            <div className="w-6/12">
               <TextInput name="city" control={control} placeholder="Cidade" />
+              <br />
               <TextInput name="uf" control={control} placeholder="Estado" />
             </div>
           </div>
         </div>
+        <br />
         <TextInput
           type="password"
           label="Senha"
@@ -103,6 +167,7 @@ export default function Register() {
           control={control}
           placeholder="Insira sua senha"
         />
+        <br />
         <TextInput
           type="password"
           label="Confirmar senha"
@@ -110,10 +175,11 @@ export default function Register() {
           control={control}
           placeholder="Confirme sua senha"
         />
+        <br />
         <Button
           onClick={handleSubmit(onSubmit)}
           variation="primary"
-          label="Acessar conta"
+          label="Criar conta"
           size="medium"
         />
       </div>
